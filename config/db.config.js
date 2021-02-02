d2fa04963ef794800c0a0005c69751058d241c41
Thet Myo Naing <thetmyo.naing@kbzbank.com>
2021-02-02 10:23:41 +0630

    database config changed

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,22 @@
 module.exports = {
-  HOST: "db-postgresql-sgp1-72946-do-user-4035363-0.db.ondigitalocean.com",
+/*   HOST: "db-postgresql-sgp1-72946-do-user-4035363-0.db.ondigitalocean.com",
   PORT: 25060,
   USER: "doadmin",
   PASSWORD: "zbr6z679r6nnxupd",
   DB: "redeem",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  } */
+
+  HOST: "db-postgresql-sgp1-27384-do-user-8629189-0.b.db.ondigitalocean.com",
+  PORT: 25060,
+  USER: "doadmin",
+  PASSWORD: "e6em9jjlh2pyrwwr",
+  DB: "defaultdb",
   dialect: "postgres",
   pool: {
     max: 5,
